f5e0c810aad34406a6312d2041c01a40be76411b
Andrija Kostic <kostic.andrija00@gmail.com>
2024-05-18 11:47:20 +0200

    add _ before state in reducer and add 2024 in title in app component

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -7,7 +7,7 @@ export const counterReducer = createReducer(
   initialState,
   on(increment, (state, action) => state + action.value),
   on(decrement, (state, action) => state - action.value),
-  on(set, (state, action) => action.value)
+  on(set, (_state, action) => action.value)
 );
 
 // old way
